363b4176cd277c7659ce87d6fcecdd956dcfe737
Atin Gupta <atingupta2005@gmail.com>
2025-12-06 12:15:26 +0530

    -

--- commands.sh
+++ commands.sh
@@ -1,6 +1,22 @@
-python -m venv venv
-venv\Scripts\activate.bat
+cd "C:\25-Trainings\2-Confirmed\11-SRE\GH"
+
+python -m venv ../venv
+
+source ../venv/Scripts/activate
+
+
 pip install -r requirements.txt
 
 mkdocs new .
+
 mkdocs serve
+
+mkdocs  build
+
+mkdocs gh-deploy
+
+mkdocs gh-deploy --force
+
+python -m mkdocs serve
+
+python -m mkdocs serve
